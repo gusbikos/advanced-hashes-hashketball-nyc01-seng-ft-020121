938b2031181a511bbbf1f69dcb33bb6e497596d8
Gus Bikos <gusbikos@gmail.com>
2021-01-22 04:27:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -144,9 +144,9 @@ end
 def shoe_size(name_of_player)
   game_hash.each do |team, team_info|
     team_info[:players].each do |player|
-      if player[:player_name] == name 
+      if player[:player_name] == name
         return player[:shoe]
-      end 
+      end
     end
   end
 end
